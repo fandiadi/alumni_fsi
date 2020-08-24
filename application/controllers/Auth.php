04e54251f50cc5c75ca125cf459e5e7931741c53
fandiadi <fandiadi12@gmail.com>
2020-08-24 07:42:06 +0700

    commit ketiga (login dan register)

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,8 +3,95 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
-        $this->load->view('auth/login');
+        $this->form_validation->set_rules('nim', 'NIM', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Halaman Login';
+            $this->load->view('template/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('template/auth_footer');
+        } else {
+            // Validasi Sukses
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $nim = $this->input->post('nim');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('tb_user', ['nim' => $nim])->row_array();
+
+        // Jika usernya ada
+        if ($user) {
+            // Jika usernya aktif
+            if ($user['is_active'] == 1) {
+                // Cek Passwordnya
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'nim' => $user['nim'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Salah!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Pengguna Belum Aktif! Harap Aktivasi Melalui Email Terdaftar</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">NIM Belum Terdaftar!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function registration()
+    {
+        // Validasi dan Notifikasi Form Register
+        $this->form_validation->set_rules('nim', 'NIM', 'required|trim|is_unique[tb_user.nim]', [
+            'is_unique' => 'NIM ini sudah terdaftar!'
+        ]);
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[4]|matches[password2]', [
+            'matches' => 'Password Tidak Sama!',
+            'min_length' => 'Password Terlalu Pendek!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        // Kembali ke Register jika kolom null
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Halaman Registrasi';
+            $this->load->view('template/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('template/auth_footer');
+        } else {
+            // Data di Input
+            $data = [
+                'nim' => $this->input->post('nim', true),
+                'nama' => htmlspecialchars($this->input->post('nama')),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 4,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('tb_user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Akun Berhasil Dibuat!</div>');
+            redirect('auth');
+        }
     }
 }
