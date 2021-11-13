196548e8312b7c6c409d5e4814057f644c0c44e8
fandiadi <fandiadi12@gmail.com>
2021-11-13 21:48:52 +0700

    update db 2

--- db/alumni_fsi.sql
+++ db/alumni_fsi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Nov 2021 pada 00.10
+-- Waktu pembuatan: 13 Nov 2021 pada 15.48
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -21,6 +21,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `alumni_fsi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_data_alumni`
+--
+
+CREATE TABLE `tb_data_alumni` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(11) NOT NULL,
+  `thn_lulus` int(4) NOT NULL,
+  `ipk` varchar(4) NOT NULL,
+  `status_alumni` varchar(20) NOT NULL,
+  `status_pekerjaan` varchar(20) NOT NULL,
+  `nama_instansi` varchar(30) NOT NULL,
+  `alamat_instansi` varchar(50) NOT NULL,
+  `bidang_ilmu` varchar(256) NOT NULL,
+  `ilmu_didapat` varchar(256) NOT NULL,
+  `kritik_saran` varchar(256) NOT NULL,
+  `create_date` int(11) NOT NULL,
+  `update_date` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_data_alumni`
+--
+
+INSERT INTO `tb_data_alumni` (`id`, `nim`, `thn_lulus`, `ipk`, `status_alumni`, `status_pekerjaan`, `nama_instansi`, `alamat_instansi`, `bidang_ilmu`, `ilmu_didapat`, `kritik_saran`, `create_date`, `update_date`) VALUES
+(7, '3411171161', 2018, '3.07', 'Bekerja', 'Kurang dari 6 Bulan', 'asd', 'asd', 'asd', 'asd', 'asd', 1636810259, 0);
 
 -- --------------------------------------------------------
 
@@ -62,13 +91,13 @@ CREATE TABLE `tb_sub_menu` (
 
 INSERT INTO `tb_sub_menu` (`id`, `id_menu`, `title`, `url`, `icon`, `is_active`) VALUES
 (1, 1, 'Home', 'admin', 'fas fa-fw fa-home', 1),
-(2, 2, 'Isi Data', 'user', 'fas fa-pen', 1),
+(2, 2, 'Isi Data', 'User/tambahdata', 'fas fa-pen', 1),
 (3, 1, 'Kelola User', 'admin', 'fas fa-fw fa-users', 1),
 (4, 2, 'Edit Profile', 'user', 'fas fa-user', 1),
 (5, 1, 'Import Data Alumni', 'import', 'fas fa-file-import', 1),
 (7, 1, 'List Data', 'admin', 'fas fa-th-list', 1),
 (8, 1, 'Rekapitulasi', 'admin', 'fas fa-file-export', 1),
-(9, 1, 'Kirim Email', 'admin', 'fas fa-mail-bulk', 1);
+(9, 1, 'Kirim Email', 'email', 'fas fa-mail-bulk', 1);
 
 -- --------------------------------------------------------
 
@@ -82,21 +111,24 @@ CREATE TABLE `tb_user` (
   `nama` varchar(30) NOT NULL,
   `email` varchar(40) NOT NULL,
   `password` varchar(256) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `date_created` int(11) NOT NULL
+  `date_created` int(11) NOT NULL,
+  `status_email` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `tb_user`
 --
 
-INSERT INTO `tb_user` (`idUser`, `nim`, `nama`, `email`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(1, '123456', 'admin', 'admin@admin.com', '$2y$10$yP0f1GJ5o8j7Cmk3jAxbOenr6.w.Uc4AkPwPeXCSJZXqJDSWarJFW', 1, 1, 1636410099),
-(2, '3411171161', 'Willy Hanafi', 'willy@hanafi.com', '$2y$10$PkOJqOzw3PdAceX7UJX5wOd8aIcoWbW5Osw9xf57QjfiMB/.Menem', 2, 1, 1598218966),
-(3, '3411171171', 'Hovi Sohibul', 'babang@tamvan.com', '$2y$10$2Vq1n/o9WIzyQExwFj1GeetbytYa4lA21HJ2H5ckAYRa2RQfa4rBu', 2, 1, 1598219932),
-(5, '3411171164', 'Christine', 'chrs@gmail.com', '$2y$10$/cIdke6gMlU1fjnTU0HQvOeQgNZ5qBNxngHSEQosumRaDx.PUfL06', 2, 1, 1598220852),
-(9, '3411171165', 'Fandi', 'fandiadi30@gmail.com', '$2y$10$I0F6yUqkCsUeaPXYZraB.O3GCkdDsNhQRMj3DBRmB/vmo4cORPWBu', 2, 1, 1636277508);
+INSERT INTO `tb_user` (`idUser`, `nim`, `nama`, `email`, `password`, `jurusan`, `role_id`, `is_active`, `date_created`, `status_email`) VALUES
+(1, '123456', 'admin', 'admin@admin.com', '$2y$10$yP0f1GJ5o8j7Cmk3jAxbOenr6.w.Uc4AkPwPeXCSJZXqJDSWarJFW', '', 1, 1, 1636410099, ''),
+(2, '3411171161', 'Willy Hanafi', 'kolab.store@gmail.com', '$2y$10$PkOJqOzw3PdAceX7UJX5wOd8aIcoWbW5Osw9xf57QjfiMB/.Menem', 'Kimia', 2, 1, 1598218966, 'Belum'),
+(3, '3411171171', 'adi', 'instalabresult@gmail.com', '$2y$10$2Vq1n/o9WIzyQExwFj1GeetbytYa4lA21HJ2H5ckAYRa2RQfa4rBu', 'Informatika', 2, 1, 1598219932, 'Belum'),
+(4, '3411171165', 'Fandi', 'fandiadi30@gmail.com', '$2y$10$I0F6yUqkCsUeaPXYZraB.O3GCkdDsNhQRMj3DBRmB/vmo4cORPWBu', 'Informatika', 2, 1, 1636277508, 'Gagal'),
+(5, '3411171111', 'tio', 'fandiadi99@gmail.com', '$2y$10$gevu4tXSRZgvrZs.Ny8IRO/sAlifRUtSbg6rZyFAsQ0UEfy175iKi', 'Kimia', 2, 1, 1636542003, 'Terkirim'),
+(6, '3411171161', 'satu', 'hanafiwilly111@gmail.com', '$2y$10$Kx.xTKtX5mYDcx3d/aLgHetmFSELXcyzmqja0rmM9Fvwrsvc5N/yW', 'Informatika', 2, 1, 1636805526, '');
 
 -- --------------------------------------------------------
 
@@ -142,6 +174,13 @@ INSERT INTO `tb_user_role` (`id`, `role`) VALUES
 --
 
 --
+-- Indeks untuk tabel `tb_data_alumni`
+--
+ALTER TABLE `tb_data_alumni`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`);
+
+--
 -- Indeks untuk tabel `tb_menu`
 --
 ALTER TABLE `tb_menu`
@@ -176,10 +215,16 @@ ALTER TABLE `tb_user_role`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `tb_data_alumni`
+--
+ALTER TABLE `tb_data_alumni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT untuk tabel `tb_menu`
 --
 ALTER TABLE `tb_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_sub_menu`
@@ -191,19 +236,19 @@ ALTER TABLE `tb_sub_menu`
 -- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user_access_menu`
 --
 ALTER TABLE `tb_user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user_role`
 --
 ALTER TABLE `tb_user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
